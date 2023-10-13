0d9926e083651f61648a99c76031705b16dddbce
Trọng Nhân Nguyễn Lê <trongnhanofficial@gmail.com>
2023-10-13 18:55:46 +0700

    Add BookStoreOData8

---
+++ BookStoreOData8/ODataBookStore/Models/DataSource.cs
@@ -0,0 +1,78 @@
+﻿using ODataBookStore.Models;
+
+namespace ODataBookStore.Data
+{
+    public static class DataSource
+    {
+        private static IList<Book> listBooks { get; set; }
+        public static IList<Book> GetBooks()
+        {
+            if(listBooks != null)
+            {
+                return listBooks;
+            }
+            listBooks = new List<Book>();
+            Book book = new Book
+            {
+                Id = 1,
+                ISBN="978-0-321-87758-1",
+                Title= "Essential C#6.0",
+                Author="Quoc An",
+                Price=59.99m,
+                Location = new Address
+                {
+                    City="HCM City",
+                    Street="D2, Thu Duc District"
+                },
+                Press = new Press
+                {
+                    Id= 1,
+                    Name="Kawasaki",
+                    Category= Category.Book,
+                }
+            };
+            listBooks.Add(book);
+            book = new Book
+            {
+                Id = 2,
+                ISBN = "978-0-321-87758-1",
+                Title = "Lam Nguoi",
+                Author = "Con Da Lap",
+                Price = 59.99m,
+                Location = new Address
+                {
+                    City = "Hau Giang",
+                    Street = "D2, Thu Duc District"
+                },
+                Press = new Press
+                {
+                    Id = 2,
+                    Name = "Honda",
+                    Category = Category.Book,
+                }
+            };
+            listBooks.Add(book);
+            book = new Book
+            {
+                Id = 3,
+                ISBN = "978-0-321-87758-1",
+                Title = "Essential C#4.0",
+                Author = "Phu Cho",
+                Price = 59.99m,
+                Location = new Address
+                {
+                    City = "Soc Trang",
+                    Street = "Tra Quyt"
+                },
+                Press = new Press
+                {
+                    Id = 3,
+                    Name = "Suzuki",
+                    Category = Category.Book,
+                }
+            };
+            listBooks.Add(book);
+            return listBooks;
+        }
+    }
+}
---
+++ BookStoreOData8/ODataBookStoreWebClient/Controllers/PressController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using ODataBookStoreWebClient.Models;
+using System.Net.Http.Headers;
+
+namespace ODataBookStoreWebClient.Controllers
+{
+    public class PressController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string ProductApiUrl = "";
+
+        public PressController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            ProductApiUrl = "https://localhost:7163/odata/Presses";
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl);
+            string strData = await response.Content.ReadAsStringAsync();
+
+            dynamic temp = JObject.Parse(strData);
+            var lst = temp.value;
+            List<Press> items = ((JArray)temp.value).Select(x => new Press
+            {
+                Id = (int)x["Id"],
+                Name = (string)x["Name"],
+            }).ToList();
+            return View(items);
+        }
+    }
+}
